ab6383cfdd767c9a170fc9808ced23503892f5cd
Kenny Dasilva <kennydasilva542@gmail.com>
2026-02-02 16:07:24 +0200

    addiction of update method

--- api/views/notification_controller.py
+++ api/views/notification_controller.py
@@ -6,18 +6,18 @@ from rest_framework import status
 from api.services.notification_service import NotificationService
 from api.serializers.notification_dto import(
     NotificationCreateDTO,
-    NotifictionResponseDTO
+    NotifictionResponseDTO,
+    NotificationUpdateDTO
 )
+from drf_yasg.utils import swagger_auto_schema
 
 
 class NoticationlistCreateController(APIView):
-
-    def get(self, request):
-        notifications=NotificationService.list_notifications()
-        serializer=NotifictionResponseDTO(notifications, many=True)
-        return Response(serializer.data)
     
-
+    @swagger_auto_schema(
+            request_body=NotificationCreateDTO,
+            responses={201: NotifictionResponseDTO}
+    )
     def post(self, request):
         dto=NotificationCreateDTO(data=request.data)
         dto.is_valid(raise_exception=True)
@@ -27,14 +27,52 @@ class NoticationlistCreateController(APIView):
 
         return Response(response.data, status=status.HTTP_201_CREATED)
     
+
+    @swagger_auto_schema(
+            request_body=NotificationCreateDTO,
+            responses={201: NotifictionResponseDTO}
+    )
+    def get(self, request):
+        notifications=NotificationService.list_notifications()
+        serializer=NotifictionResponseDTO(notifications, many=True)
+        return Response(serializer.data)
+
+
+    @swagger_auto_schema(responses={204: "No Content"})  
     def delete(self, request, id):
         notifiction=NotificationService.remove_notifications(id)
         serializer=NotifictionResponseDTO(notifiction)
         return Response(serializer.data)
+    
+    @swagger_auto_schema(
+            request_body=NotificationUpdateDTO,
+            responses={200: NotifictionResponseDTO}
+    ) 
+    def patch(self, request, id):
+        dto=NotificationUpdateDTO(data=request.data)
+        dto.is_valid(raise_exception=True)
+
+        notification=NotificationService.update(id, dto.validated_data)
+
+        if not notification:
+            return Response(
+                {"detail": "Notification not found."},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        
+        Response=NotifictionResponseDTO(notification)
+        return Response(Response.data)
+    
+    
 
 
 class NotificationMarkAsSentController(APIView):
 
+
+    @swagger_auto_schema(
+            
+            responses={200: NotifictionResponseDTO}
+    ) 
     def patch(self, request, id):
         notifiction=NotificationService.mark_as_sent(id)
         serializer=NotifictionResponseDTO(notifiction)
